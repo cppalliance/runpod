eb02b641f9b8eaca862f5b28f8413d08a0f99169
sdarwin <samuel.d.darwin@gmail.com>
2026-07-06 22:49:51 +0000

    vllm-openai-v0-24-0

---
+++ docker/vllm-openai-v0-24-0/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -xe
+image=cppalliance/vllm-openai:v0.24.0
+echo image is $image
+docker buildx build -t $image .
+
+echo $?
+# if [ "$?" = "0" ] ; then
+#     docker push $image
+# fi
